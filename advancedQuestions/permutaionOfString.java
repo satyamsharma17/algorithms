05fa352324100041abfc3dad39e80bc22aac73f8
Satyam Sharma <72562562+satyamsharma17@users.noreply.github.com>
2021-06-19 15:44:07 +0530

    Add files via upload

---
+++ advancedQuestions/permutaionOfString.java
@@ -0,0 +1,38 @@
+package algorithms.advancedQuestions;
+
+import java.util.*;
+
+public class permutaionOfString {
+	
+	static Set<String> set = new HashSet<>();
+
+	public static void main(String[] args) {
+		
+		permutations("abcc", 0, 3);
+
+	}
+ 	
+	static void  permutations(String s, int l, int r) {
+		if(l == r) {
+			if(set.contains(s)) return;
+			set.add(s);
+			System.out.println(s);
+			return;
+		}
+		
+		for(int i = l; i <= r; i++) { 
+			 s = interchangeChar(s, l, i);
+			 permutations(s, l+1, r);
+			 s = interchangeChar(s, l, i);
+		}
+	}
+	
+	static String interchangeChar(String s, int a, int b) {
+		char array[] = s.toCharArray();
+		char temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
+		return String.valueOf(array);
+	}
+
+}
